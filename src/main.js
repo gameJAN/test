b3ecb9fcea1eb54bf48535810dca80d66c249b50
jan <980880532@qq.com>
2020-10-20 09:30:38 +0800

    eighthTest

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from "./router"
+import App from "./App.vue"
+import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
-console.log(1);
 new Vue({
   router,
   store,
